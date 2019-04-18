7a8fc32a12630e1ce45c3a135256dd3d5866defc
Aatoken <3410760440@qq.com>
2019-04-18 23:59:38 +0800

    项目的初始化创建完成，1，实现游戏窗体

--- Game-Tank/src/main/kotlin/MyWindow.kt
+++ Game-Tank/src/main/kotlin/MyWindow.kt
@@ -1,2 +1,33 @@
-class MyWindow {
+import javafx.application.Application
+import javafx.scene.input.KeyEvent
+import org.itheima.kotlin.game.core.Window
+
+/**
+ * Auther Aatoken
+ * Date 2019/4/18
+ * Des extend游戏引擎的窗体
+ */
+class MyWindow : Window() {
+
+
+
+    override fun onDisplay() {
+          //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onKeyPressed(event: KeyEvent) {
+         //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onRefresh() {
+         //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreate() {
+         //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+fun main(args: Array<String>) {
+    Application.launch(MyWindow::class.java)
 }
